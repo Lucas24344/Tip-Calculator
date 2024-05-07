df6c6e501ca1959b297df5fc7ee9d0e1cb829aae
Lucas Sousa <lucassousagth@gmail.com>
2024-05-07 11:49:06 -0300

    Removido arquivos desnecessários

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function validar(valor){
     else{
         document.getElementById("err").style.display="block";
     }
-}
+}amount
 
 function calc(valor){
     var valorGorjeta=valor.value;
@@ -26,13 +26,12 @@ function calc(valor){
     document.getElementById("err2").style.display="none";
     document.getElementById("err").style.display="none";
 
-
     let valorConta = parseFloat(document.getElementById("bill").value);
     let numeroPessoas = parseFloat(document.getElementById("person").value);
     let total = document.getElementById("total");
     let amount = document.getElementById("amount");
 
-    let totalGorjeta= (valorConta * valorGorjeta) /100;
+    let totalGorjeta = (valorConta * valorGorjeta) /100;
     let sum = valorConta + totalGorjeta;
     let totalPorPessoa=sum/numeroPessoas;
     amount.innerHTML="$" + totalGorjeta/numeroPessoas.toFixed(2)
@@ -44,10 +43,7 @@ function reset(){
     let bill = document.getElementById("bill").value;
     let people = document.getElementById("person").value;
     
-
     if(bill!=="" && people!==""){
         location.reload();
     }
 }
-
-
